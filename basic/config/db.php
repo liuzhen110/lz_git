464337394081cd540d825dd856f72b4a34711a0c
刘震 <liuzheng@example.com>
2016-03-24 13:26:27 +0800

    liuzhen del basic

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dashixun',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
